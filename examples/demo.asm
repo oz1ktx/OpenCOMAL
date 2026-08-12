d7ef50ede6be1d8a545370411b0392b111dc08de
Peter N Andreasen <peter.noesgaard.andreasen@prevas.dk>
2026-08-12 11:19:20 +0200

    Update project status and add assembly demo

---
+++ examples/demo.asm
@@ -0,0 +1,13 @@
+; OpenCOMAL Z80 demo program
+; Prints a message via CP/M BDOS (function 9), then exits (function 0)
+
+        ORG 0100h
+
+START:  LD DE,MSG
+        LD C,9
+        CALL 5
+
+        LD C,0
+        CALL 5
+
+MSG:    DB 'HELLO FROM OPENCOMAL Z80 DEMO$'
